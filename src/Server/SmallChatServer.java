8d02bb40246803c48937235704c7ff220e9edaf9
Michael Melchione <michaelmelchione@gmail.com>
2013-05-29 10:57:52 -0400

    Server implementation
    Began to implement the server. I did this using Sockets and Threads.
    Two new classes were created, ClientThread() and SmallChatServer() in
    the Server package. The code compiles but I get an error very soon
    before the connection can be made. The connection attempts to connect
    with localhost at port 4242 (random port).
    
    Many new methods were added and I deleted a few that were previously
    there. All new methods are commented.

---
+++ src/Server/SmallChatServer.java
@@ -0,0 +1,77 @@
+package Server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SmallChatServer implements Runnable{
+
+	private ServerSocket server = null;
+	private Thread thread = null;
+	private smalltalkServer client = null;
+	
+	/*
+	 * Constructor for SmallChatServer
+	 * Parameters - port as an integer
+	 * Binds to port and begins the server
+	 */
+	public SmallChatServer(int port){
+		try{
+			System.out.println("Binding to port " + port + ", please wait...");
+			server = new ServerSocket(port);
+			System.out.println("Server started: " + server);
+			start();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	/*
+	 * Method that starts the thread
+	 */
+	private void start(){
+		if (thread == null){
+			thread = new Thread(this); 
+			thread.start();
+		}	
+	}
+	
+	/*
+	 * Method that stops the thread
+	 */
+	public void stop(){  
+		if (thread != null){
+		   thread.stop(); 
+		   thread = null;
+		}
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		while(thread != null){
+			try{
+				System.out.println("Waiting for client...");
+				addThread(server.accept());
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	/*
+	 * Adds a new client when needed
+	 * Parameters - Socket
+	 * Accepts client and starts their thread
+	 */
+	public void addThread(Socket socket){
+		System.out.println("Accepting client...");
+		client = new smalltalkServer(this, socket);
+		try{
+			client.open();
+			client.start();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
